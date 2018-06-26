f76623487b7ff087da60e9e7082a9b44541b8f9b
ahmedwadod <ahmedwdood@hotmail.com>
2018-06-26 15:27:33 +0200

    Updated MazeBot

--- MazeBot/MazeBot/Properties/AssemblyInfo.cs
+++ MazeBot/MazeBot/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MazeBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Robot that solves simple maze")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MazeBot")]
